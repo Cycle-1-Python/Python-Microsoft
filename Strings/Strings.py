e63508c16e41ef68a4f03ade519836aa31be2fad
Gus <biersackandy1990@gmail.com>
2022-07-07 10:50:33 -0500

    Working with strings

---
+++ Strings/Strings.py
@@ -0,0 +1,21 @@
+first_name = 'Chirstopher'
+print(first_name)
+
+# You can combine strings with +
+first_name = 'Christopher'
+last_name ='Harrison'
+print(first_name + last_name)
+print('Hello ' + first_name + ' ' + last_name)
+
+# You can use functions to modify strings
+sentence = 'The dog is named Sammy'
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.capitalize())
+print(sentence.count('a'))
+print(sentence.count('m'))
+
+# The functions help us format strings we save to files and databases, or display to users
+first_name = input('What is your first name? ')
+last_name = input('What is your last name? ')
+print('Hello ' + first_name.capitalize() + ' ' + last_name.capitalize())
