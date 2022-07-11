14867942582cb112b2346663cbc4f24c840f8bf2
Gus <biersackandy1990@gmail.com>
2022-07-11 14:20:36 -0500

    Working with dates and conditions

---
+++ error_handling.py
@@ -0,0 +1,16 @@
+from ast import Try
+
+
+x = 42
+y = 0
+
+print()
+try:
+    print(x / y)
+except ZeroDivisionError as e:
+    print('Not allowed to divide by zero')
+else:
+    print('Something else went wrong')    
+finally:
+    print('This is our cleanup code')
+print()
